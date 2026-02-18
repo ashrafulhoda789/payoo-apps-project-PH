d7a07e71a60cd7bf521792feec3695c073fcc5b7
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-19 00:10:16 +0600

    login page designed and add functionality

---
+++ script/login.js
@@ -0,0 +1,27 @@
+document.getElementById('login-btn').addEventListener('click',
+    function(){
+        //1- get the mobile number input
+        const numberInput = document.getElementById('input-number');
+        const contactNumber = numberInput.value;
+        console.log(contactNumber);
+
+        //2- get the pin
+        const inputPin = document.getElementById('input-pin');
+        const pin = inputPin.value;
+        console.log(pin);
+
+        //3- match pin and mobile number
+        if(contactNumber === "01818299406" && pin === '1234'){
+            // 3-1 true:::>> alert > homepage 
+            alert("Login Successfull");
+            // window.location.replace("/home.html")
+            window.location.assign("/home.html")
+        }else{
+            // 3-1 false:::>> alert > return 
+            alert("Login Failed");
+            return;
+        }
+        
+       
+    }
+)
